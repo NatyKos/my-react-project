ede8cc0ed99e4dcd918a4832a7642804075e16a8
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-03-31 10:58:10 +0100

    .

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import Product from './Product';
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+
+
